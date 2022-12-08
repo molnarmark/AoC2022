754bef5362fb45525a96c770958c4b8fd6f75b35
Mark Molnar <molnarpw@gmail.com>
2022-12-09 00:31:20 +0100

    Fix day 4 to proper (but bad) solution

--- 04/solution.swift
+++ 04/solution.swift
@@ -5,30 +5,16 @@ let data = try String(contentsOfFile: "input.txt", encoding: .utf8).components(s
 func partOne() -> Int {
   var answer = 0
 
-  let pairs = data.map{ line in (
-    line.components(separatedBy: ",")[0].components(separatedBy: "-").map { Int($0)! }.first!,
-    line.components(separatedBy: ",")[1].components(separatedBy: "-").map { Int($0)! }.first!
-  )}.map { Set($0...$1) }
-  print(pairs)
+  for line in data {
+    let section1RawValues = line.components(separatedBy: ",")[0].components(separatedBy: "-").map { Int($0)! }
+    let section2RawValues = line.components(separatedBy: ",")[1].components(separatedBy: "-").map { Int($0)! }
+    let section1 = Set(section1RawValues[0]...section1RawValues[1])
+    let section2 = Set(section2RawValues[0]...section2RawValues[1])
 
-  // let pairs = data.map{ line in (
-  //   line.components(separatedBy: ",")[0].components(separatedBy: "-").map{ (left, right)  in (Set(Int(left)!...Int(right)!)) },
-  //   line.components(separatedBy: ",")[0].components(separatedBy: "-").map{ (left, right) in  (Set(Int(left)!...Int(right)!)) }
-  // )}
-
-  // print(pairs)
-  // print(pairs.map{ $0.isSubset(of: $1) || $1.isSubset(of: $0) }.filter { !$0 }.count)
-
-  // for line in data {
-  //   let section1RawValues = line.components(separatedBy: ",")[0].components(separatedBy: "-").map { Int($0)! }
-  //   let section2RawValues = line.components(separatedBy: ",")[1].components(separatedBy: "-").map { Int($0)! }
-  //   let section1 = Set(section1RawValues[0]...section1RawValues[1])
-  //   let section2 = Set(section2RawValues[0]...section2RawValues[1])
-
-  //   if (section1.isSubset(of: section2) || section2.isSubset(of: section1)) {
-  //     answer += 1
-  //   }
-  // }
+    if (section1.isSubset(of: section2) || section2.isSubset(of: section1)) {
+      answer += 1
+    }
+  }
 
   return answer
 }
@@ -49,5 +35,5 @@ func partTwo() -> Int {
   }.filter{ $0 }.count
 }
 
-print(partOne() == 524)
+print(partOne())
 print(partTwo())
