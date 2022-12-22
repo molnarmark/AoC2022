51e181272aeb09d9161037757bfea8ba03b3761b
Mark Molnar <molnarpw@gmail.com>
2022-12-22 15:03:29 +0100

    Day 8 part 2

--- 08/solution.swift
+++ 08/solution.swift
@@ -9,7 +9,9 @@ func calculateToEdgeLeft(row: Int, column: Int) -> [Int] {
 }
 
 func calculateToEdgeRight(row: Int, column: Int) -> [Int] {
-  return Array(stride(from: column + 1, through: grid.count - 1, by: 1)).map { Int(String(grid[row][$0]))! }
+  return Array(stride(from: column + 1, through: grid.count - 1, by: 1)).map {
+    Int(String(grid[row][$0]))!
+  }
 }
 
 func calculateToEdgeTop(row: Int, column: Int) -> [Int] {
@@ -17,9 +19,10 @@ func calculateToEdgeTop(row: Int, column: Int) -> [Int] {
 }
 
 func calculateToEdgeBottom(row: Int, column: Int) -> [Int] {
-  return Array(stride(from: row + 1, through: grid.count - 1, by: 1)).map { Int(String(grid[$0][column]))! }
+  return Array(stride(from: row + 1, through: grid.count - 1, by: 1)).map {
+    Int(String(grid[$0][column]))!
+  }
 }
-
 
 func partOne() -> Int {
   var answer = outerVisible
@@ -27,16 +30,42 @@ func partOne() -> Int {
   for i in 1...grid.count - 2 {
     for j in 1...grid.count - 2 {
       let condition: (Int) -> Bool = { return $0 < Int(String(grid[i][j]))! }
-      answer += [
-        calculateToEdgeLeft(row: i, column: j).allSatisfy(condition),
-        calculateToEdgeRight(row: i, column: j).allSatisfy(condition),
-        calculateToEdgeTop(row: i, column: j).allSatisfy(condition),
-        calculateToEdgeBottom(row: i, column: j).allSatisfy(condition)
-      ].contains(true) ? 1 : 0
+      answer +=
+        [
+          calculateToEdgeLeft(row: i, column: j).allSatisfy(condition),
+          calculateToEdgeRight(row: i, column: j).allSatisfy(condition),
+          calculateToEdgeTop(row: i, column: j).allSatisfy(condition),
+          calculateToEdgeBottom(row: i, column: j).allSatisfy(condition),
+        ].contains(true) ? 1 : 0
     }
   }
 
   return answer
 }
 
-print(partOne())
+func partTwo() -> Int {
+  var highScore = 0
+  for i in 1...grid.count - 2 {
+    for j in 1...grid.count - 2 {
+      let value = Int(String(grid[i][j]))!
+      let score = [
+        calculateToEdgeLeft(row: i, column: j),
+        calculateToEdgeRight(row: i, column: j),
+        calculateToEdgeTop(row: i, column: j),
+        calculateToEdgeBottom(row: i, column: j),
+      ].map { direction -> Int in
+        if let index = direction.firstIndex(where: { $0 >= value }) {
+          return index + 1
+        }
+        return direction.count
+      }.reduce(1, *)
+
+      highScore = max(highScore, score)
+    }
+  }
+
+  return highScore
+}
+
+print("Part One:", partOne())
+print("Part Two:", partTwo())
